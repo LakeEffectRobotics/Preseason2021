5891722c298ceabbd4e6d5b762cd00e5b052614b
gbuckholtz <gbuckholtz@gmail.com>
2021-11-17 15:49:17 -0500

    testing linux push

--- src/main/java/ler/robot/Robot.java
+++ src/main/java/ler/robot/Robot.java
@@ -1,8 +1,6 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-// The Garbage  Bot!
-// another comment
 
 package ler.robot;
 
